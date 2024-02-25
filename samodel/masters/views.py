4ebc8cfc814e8e8c5d6dba7388b282cf2f905fed
hammer <ttkagentusolie@gmail.com>
2024-02-25 19:33:22 +0800

    redirect

--- samodel/masters/views.py
+++ samodel/masters/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
@@ -16,7 +16,7 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2024:
-        raise Http404()
+        return redirect('/')
     return HttpResponse(f'<h1>Архив по годам</h1><p>year: {year}</p>')
 
 
