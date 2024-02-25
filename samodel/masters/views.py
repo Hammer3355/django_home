ad714247a8afa7eb96bde21006ac3a08270a3f1a
hammer <ttkagentusolie@gmail.com>
2024-02-26 02:27:03 +0800

    Передача данных в шаблон

--- samodel/masters/views.py
+++ samodel/masters/views.py
@@ -4,14 +4,30 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 def index(request):
     # t = render_to_string('masters/index.html')
     # return HttpResponse(t)
-    return render(request, 'masters/index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20)
+    }
+    return render(request, 'masters/index.html', context=data)
 
 def about(request):
-    return render(request, 'masters/about.html')
+    return render(request, 'masters/about.html', {'title': 'О сайте'})
 
 
 
