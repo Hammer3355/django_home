a7cc18e40eef68b091e28b82ef7ec53e906c8725
hammer <ttkagentusolie@gmail.com>
2024-02-26 14:32:55 +0800

    Циклы и условия в шаблоне

--- samodel/masters/views.py
+++ samodel/masters/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
-
 
 data_db = [
     {'id': 1, 'title': 'Андженина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+    {'id': 4, 'title': 'Евгений Сахаров', 'content': 'Биография Евгения Сахарова', 'is_published': True},
 ]
+
 
 def index(request):
     # t = render_to_string('masters/index.html')
@@ -19,17 +19,13 @@ def index(request):
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'float': 28.56,
-        'lst': [1, 2, 'abc', True],
-        'set': {1, 2, 3, 2, 5},
-        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10, 20)
+        'posts': data_db
     }
     return render(request, 'masters/index.html', context=data)
 
+
 def about(request):
     return render(request, 'masters/about.html', {'title': 'О сайте'})
-
 
 
 def categories(request, cat_id):
@@ -42,7 +38,7 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2024:
-        uri = reverse('cats', args=('music', ))
+        uri = reverse('cats', args=('music',))
         return redirect(uri)
 
     return HttpResponse(f'<h1>Архив по годам</h1><p>year: {year}</p>')
